01e81e3dcef40a65128cebe06ca75898eb328da8
Fandikoos <danfandos@gmail.com>
2023-03-10 11:47:01 +0100

    Cargamos el Chrome driver y las librerias que utilizaremos para este ejercicio

--- src/main/java/Tests/TestBarraDeBusqueda.java
+++ src/main/java/Tests/TestBarraDeBusqueda.java
@@ -1,4 +1,14 @@
 package Tests;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestBarraDeBusqueda {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\danfa\\Desktop\\TestFuncionalLidl\\TestFuncionalLidl\\src\\main\\resources\\driver\\chromedriver.exe");
+
+    }
+
 }
