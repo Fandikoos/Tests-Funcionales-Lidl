0155d4cb294fcc396a9828e9dcaf16108129e057
Fandikoos <danfandos@gmail.com>
2023-03-10 11:38:02 +0100

    Cargamos la dependencia de selenium dentro de nuestro archivo pom.xml y cargamos el driver de Chrome dentro de nuestra clase TestListarProductos

--- src/main/java/Tests/TestListarProductos.java
+++ src/main/java/Tests/TestListarProductos.java
@@ -1,7 +1,9 @@
 package Tests;
 
+
 public class TestListarProductos {
     public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\danfa\\Desktop\\TestFuncionalLidl\\TestFuncionalLidl\\src\\main\\resources\\driver\\chromedriver.exe");
 
     }
 }
