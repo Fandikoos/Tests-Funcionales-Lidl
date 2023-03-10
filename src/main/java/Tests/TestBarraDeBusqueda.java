063102d58479b3723ae4ea7cbed149fb1f7b2885
Fandikoos <danfandos@gmail.com>
2023-03-10 11:51:28 +0100

    Se realiza segundo test en el que buscamos un producto en la barra de búsqueda de Lidl, en este caso, vino

--- src/main/java/Tests/TestBarraDeBusqueda.java
+++ src/main/java/Tests/TestBarraDeBusqueda.java
@@ -9,6 +9,30 @@ public class TestBarraDeBusqueda {
 
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\danfa\\Desktop\\TestFuncionalLidl\\TestFuncionalLidl\\src\\main\\resources\\driver\\chromedriver.exe");
 
+        //Inicializamos browser
+        WebDriver webDriver = new ChromeDriver();
+
+        // Creamos la nueva instancia
+        WebDriver driver = new ChromeDriver();
+
+        // Navegamos a la pagina de lidl
+        driver.get("https://www.lidl.es");
+
+        //Aceptar las cookies
+        driver.findElement(By.className("cookie-alert-extended-button")).sendKeys("cookie-alert-extended-button"+ Keys.ENTER);
+
+        //Busque algo en la barra de búsqueda
+        driver.findElement(By.id("mainsearch-input")).sendKeys("vino" + Keys.ENTER);
+
+        // Esperamos a que cargue la pagina y cargamos una excepción por si surgiera algún error
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //Pintamos la pagina
+        System.out.println("Page title is: " + driver.getTitle());
     }
 
 }
