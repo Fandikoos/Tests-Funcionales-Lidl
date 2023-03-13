0343b2166c616e3bdf8e501058945c1f9fdc0797
Fandikoos <danfandos@gmail.com>
2023-03-13 11:10:36 +0100

    Cargamos las librería y el driver

--- src/main/java/Tests/TestCarritoCompra.java
+++ src/main/java/Tests/TestCarritoCompra.java
@@ -1,4 +1,13 @@
 package Tests;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestCarritoCompra {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\danfa\\Desktop\\TestFuncionalLidl\\TestFuncionalLidl\\src\\main\\resources\\driver\\chromedriver.exe");
+
+    }
+
 }
