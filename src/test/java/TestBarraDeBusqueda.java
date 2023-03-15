7a88aaf1451ea9b4cd60488761475d3f4fb78c11
Fandikoos <danfandos@gmail.com>
2023-03-15 10:33:08 +0100

    Se añade TestBarraDeBusqueda para comprobar que se cumple correctamente, aparte se eliminan los driver.quit para que se vea mejor el despliege

--- src/test/java/TestBarraDeBusqueda.java
+++ src/test/java/TestBarraDeBusqueda.java
@@ -7,31 +7,31 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-
-
-public class TestListarProductos {
+public class TestBarraDeBusqueda {
 
     @Test
-    public void listarProductos(){
+    public void BarraDeBusqueda(){
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\danfa\\Desktop\\TestFuncionalLidl\\TestFuncionalLidl\\src\\main\\resources\\driver\\chromedriver.exe");
-
-        //Este test consistira en listar las panificadoras que se venden en Lidl
 
         // Creamos la nueva instancia
         WebDriver driver = new ChromeDriver();
 
         // Navegamos a la pagina de lidl
-        driver.get("https://www.lidl.es/es/panificadoras/c6229");
+        driver.get("https://www.lidl.es");
 
         //Aceptar las cookies
-        WebElement element = driver.findElement(By.className("cookie-alert-extended-button"));
+        driver.findElement(By.className("cookie-alert-extended-button")).sendKeys("cookie-alert-extended-button"+ Keys.ENTER);
+
+        //Busque algo en la barra de búsqueda
+        WebElement element = driver.findElement(By.id("mainsearch-input"));
 
         //Inicializamos un timeout a traves del wait de selenium
         WebDriverWait wait = new WebDriverWait(driver, 10);
         wait.until(ExpectedConditions.visibilityOf(element));
 
-        element.sendKeys("cookie-alert-extended-button"+ Keys.ENTER);
+        element.sendKeys("vino" + Keys.ENTER);
+
+        //Pintamos la pagina
+        System.out.println("Page title is: " + driver.getTitle());
     }
-
-
 }
